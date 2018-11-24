aa5aa16edb6861ca1ea78889ecba10480ec2fcbd
Alaa Moucharrafie <alaa.moucharrafie@gmail.com>
2018-11-24 11:30:39 +0100

    [spotify] get auth state and redir url

--- src/spotify.py
+++ src/spotify.py
@@ -1,9 +1,15 @@
 import base64
 import datetime
 import json
+import random
 import requests
+import string
+import urllib.parse
 
-SPOTIFY_BASE_URL = 'https://accounts.spotify.com{}'
+SPOTIFY_API_BASE_URL = 'https://accounts.spotify.com{}'
+SPOTIFY_SRV_BASE_URL = 'https://localhost{}'
+SPOTIFY_REDIRECT_URI = SPOTIFY_SRV_BASE_URL.format('/callback')
+SPOTIFY_AUTH_SCOPES = ['playlist-modify-public', 'user-top-read']
 SPOTIFY_CLIENT_ID = '1a47f705dfca49b09c7ea6fec6070b8d'
 SPOTIFY_CLIENT_SECRET = '8546bb88f48541f585f208c3b86c6f33'
 SPOTIFY_ACCESS_TOKEN = None
@@ -14,7 +20,7 @@ def __str2bytes(str):
 def __bytes2str(byt):
     return byt.decode("utf-8")
 
-def do_request(url, method='get', params=None, headers=None):
+def do_request(url, method='get', params=None, headers=None, allow_redirects=False):
     _method = requests.get
     if method == 'post':
         _method = requests.post
@@ -23,37 +29,36 @@ def do_request(url, method='get', params=None, headers=None):
     print('params: {params}'.format(params=params))
     print('headers: {headers}'.format(headers=headers))
     print('------')
-    request = _method(url, params=params, headers=headers)
+    request = _method(url, params=params, headers=headers, allow_redirects=allow_redirects)
     print('------')
+    print('headers: {headers}'.format(headers=sorted(request.headers)))
     print('response: {status_code}'.format(status_code=request.status_code))
     print('{content}'.format(content=request.content))
     print('------')
     return (request.status_code, request.content)
 
-def spotify_request_auth_token():
-    status_code, content = do_request(
-        SPOTIFY_BASE_URL.format('/authorize'),
-        params={
-            'client_id': SPOTIFY_CLIENT_ID,
-            'response_type': 'code',
-            'redirect_uri': 'https://localhost',
-            'scope': 'playlist-modify-public user-top-read'
-        }
+def get_redir_url(auth_state):
+    raw_params = {
+        'client_id': SPOTIFY_CLIENT_ID,
+        'redirect_uri': SPOTIFY_REDIRECT_URI,
+        'scope': ' '.join(SPOTIFY_AUTH_SCOPES),
+        'state': auth_state,
+        'response_type': 'code'
+    }
+    params = '&'.join('{key}={value}'.format(
+        key=key,
+        value=urllib.parse.quote(value, safe='')) for key, value in raw_params.items()
     )
-    if status_code == 200:
-        print('------')
-        print(content)
-        print('------')
-    else:
-        print('------')
-        print('ERROR: status code {}'.format(status_code))
-        print(content)
-        print('------')
+    return SPOTIFY_API_BASE_URL.format('/authorize?{params}'.format(params=params))
 
+def get_new_auth_state(
+        size=8,
+        charset=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+    return ''.join(random.choice(charset) for _ in range(size))
 
 def spotify_request_access_token():
     status_code, content = do_request(
-        SPOTIFY_BASE_URL.format('/api/token'),
+        SPOTIFY_API_BASE_URL.format('/api/token'),
         'post',
         params={
             'grant_type': 'client_credentials'
@@ -91,4 +96,7 @@ def spotify_request_access_token():
         print('------')
 
 if __name__ == '__main__':
-    spotify_request_auth_token()
+    state = get_new_auth_state()
+    print(state)
+    print(get_redir_url(state))
+#    spotify_request_auth_token()
