5afbf05d63f21662c00b820093d17ae5de46c150
Alaa Moucharrafie <alaa.moucharrafie@gmail.com>
2018-11-24 08:35:06 +0100

    [spotify] basic access token request implementation

--- src/Spotify.py
+++ src/Spotify.py
@@ -1,78 +1,73 @@
-
+import base64
+import datetime
 import json
-import sys 
 import requests
-import spotipy.util as util
 
-#CLIENT KEYS 
-CLIENT_ID = '1a47f705dfca49b09c7ea6fec6070b8d'
-CLIENT_SECRET = '8546bb88f48541f585f208c3b86c6f33'
-REDIRECT_URI= 'http://localhost/'
+SPOTIFY_BASE_URL = 'https://accounts.spotify.com{}'
+SPOTIFY_CLIENT_ID = '1a47f705dfca49b09c7ea6fec6070b8d'
+SPOTIFY_CLIENT_SECRET = '8546bb88f48541f585f208c3b86c6f33'
+SPOTIFY_ACCESS_TOKEN = None
+SPOTIFY_TOKEN_EXPIRE = None
 
+def __str2bytes(str):
+    return str.encode("utf-8")
+def __bytes2str(byt):
+    return byt.decode("utf-8")
 
-# Spotify URLS
-SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+def do_request(url, method='get', params=None, headers=None):
+    _method = requests.get
+    if method == 'post':
+        _method = requests.post
+    print('------')
+    print('request: {method} on {url}'.format(method=method, url=url))
+    print('params: {params}'.format(params=params))
+    print('headers: {headers}'.format(headers=headers))
+    print('------')
+    request = _method(url, params=params, headers=headers)
+    print('------')
+    print('response: {status_code}'.format(status_code=request.status_code))
+    print('{content}'.format(content=request.content))
+    print('------')
+    return (request.status_code, request.content)
 
+def spotify_request_access_token():
+    status_code, content = do_request(
+        SPOTIFY_BASE_URL.format('/api/token'),
+        'post',
+        params={
+            'grant_type': 'client_credentials'
+        },
+        headers={
+            'Authorization': 'Basic {}'.format(
+                __bytes2str(base64.b64encode(
+                    __str2bytes('{}:{}'.format(
+                        SPOTIFY_CLIENT_ID,
+                        SPOTIFY_CLIENT_SECRET
+                    ))
+                ))
+            ),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    )
+    if status_code == 200:
+        content = json.loads(__bytes2str(content))
+        duration_seconds = int(content['expires_in'])
+        expire_date = datetime.datetime.now() + datetime.timedelta(0, duration_seconds)
 
+        SPOTIFY_ACCESS_TOKEN = content['access_token']
+        SPOTIFY_TOKEN_EXPIRE = expire_date
 
+        print('------')
+        print('Good! Access token will expire in {duration}s, or roughly at {timestamp}'.format(
+            duration=duration_seconds,
+            timestamp=expire_date
+        ))
+        print('------')
+    else:
+        print('------')
+        print('ERROR: status code {}'.format(status_code))
+        print(content)
+        print('------')
 
-def _make_http_request(url, method='get', params=None, headers=None, data=None, auth=None):
-    request_method = requests.post if method == 'post' else requests.get
-    res = request_method(url, params=params, headers=headers, data=data, auth=auth)
-    responsejson = res.json()
-    return responsejson
-
-
-def get_track(track_id, url):
-    url = url + album_id +'/tracks'
-    resp = requests.get(url)
-    return resp.json()
-
-def get_several_tracks(list_of_ids, url):
-    resp = requests.get(url)
-    return resp.json()
-
-
-#https://api.spotify.com/v1/albums/{id}/tracks
-def get_albums_tracks(album_id, url='https://api.spotify.com/v1/albums/'):
-    url = url + album_id +'/tracks'
-    resp = requests.get(url)
-    return resp.json()
-
-
-
-def get_users_playlists(auth_header, url):
-    url = USER_PLAYLISTS_ENDPOINT
-    resp = requests.get(url, headers=auth_header)
-    return resp.json()
-
-
-def get_spotify_artist(name, access_token):
-    headers = {'Authorization': 'Bearer ' + access_token,}
-    params = (('q', name), ('type', 'artist'),)
-    res = _make_http_request('https://api.spotify.com/v1/search', headers=headers, params=params)
-    items = res.get('artists', {}).get('items', None)
-
-
-def request_access_token(client_id, client_secret):
-    headers = {'Accept': 'application/json'}
-    data = [('grant_type', 'client_credentials')]
-    res = _make_http_request(SPOTIFY_TOKEN_URL, method='post', data=data, auth=(client_id, client_secret))
-    return res['access_token']
-
-
-
-def init():
-    #user auth
-    #code = request_authorization(CLIENT_ID,CLIENT_SECRET)
-    #once the user has logged in
-    token = request_access_token(CLIENT_ID,CLIENT_SECRET)
-    print(token)
-    artist = get_spotify_artist('Lorde', token)
-
-
-
-if __name__ == "__main__":
-   init()
+if __name__ == '__main__':
+    spotify_request_access_token()
