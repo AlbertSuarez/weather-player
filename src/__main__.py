4cfbce8e3ca1b7b8394144ed3c0c291b8cd28093
Alaa Moucharrafie <alaa.moucharrafie@gmail.com>
2018-11-25 05:28:02 +0100

    removed darksky from __main__

--- src/__main__.py
+++ src/__main__.py
@@ -2,10 +2,6 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from src.player import flask_app
-from src.darksky import darksky
-
-print(darksky.get_forecast())
-
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', port=8081)
